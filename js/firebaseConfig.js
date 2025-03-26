91a4930c21da6050c673ba2082f55a753b63da58
Art Group <artgroup1995@gmail.com>
2025-03-26 11:33:14 +0530

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,9 +1,9 @@
 // Firebase imports and initialization
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-database.js";
 
 const appSetting = {
-    databaseURL: "https://art-hr-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://art-hr-b72b9-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(appSetting);
